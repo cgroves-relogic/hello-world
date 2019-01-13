7d3a77bd9587f188a382535af244834ee946b34b
cgroves-relogic <40256693+cgroves-relogic@users.noreply.github.com>
2019-01-13 14:35:55 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World! Nice to see you..");
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
